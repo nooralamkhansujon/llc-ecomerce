3718f255feb97558d68ab8a11300cdcdf1f96501
nooralam <nooralam@gmail.com>
2019-09-16 22:57:03 +0600

    User table migrations complete

--- database/migrations/2019_09_16_162152_create_users_table.php
+++ database/migrations/2019_09_16_162152_create_users_table.php
@@ -15,6 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name',128);
+            $table->string('email',128)->unique();
+            $table->string('phone_number',32)->unique();
+            $table->string('password',128);
+            $table->bigInteger('reward_points')->default(0);
+            $table->date('email_verified_at')->nullable();
+            $table->string('email_verification_token',100)->nullable();
+            $table->string('facebook_id',32)->nullable();
+            $table->string('google_id',32)->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
